a4b8e34b0335aa5887eb4423032f737092dc7044
domheri <47629857+domheri@users.noreply.github.com>
2019-03-18 13:55:14 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -952,21 +952,6 @@ client.on('guildMemberAdd', member => {
    
       channel.sendEmbed(embed);
     });
-    
-    client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`لقد خرج صديق :( :raised_hand::skin-tone-1: :pensive:`)
-        .setDescription(`مع السلامة يحبي :raised_hand::skin-tone-1: :pensive: `)
-        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('RED')
-        .setFooter(`====شكرا للاستمتاع====`, ' https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-    
-    var channel =member.guild.channels.find('name', 'welcome')
-    if (!channel) return;
-    channel.send({embed : embed});
-    })
 
 
 client.on('message', message => {
